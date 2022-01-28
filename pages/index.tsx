1e0e364228f9ef86fdcb709bbaadc80337337fe1
Maximilian Lindsey <max@lindsey.digital>
2022-01-28 22:28:44 +0100

    Render playlist images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import Head from "next/head";
+import { useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { PlaylistType } from "../lib/spotify-types";
 
 export default function Home() {
   const { data: session } = useSession();
+  const [list, setList] = useState([]);
+
+  const getMyPlaylists = async () => {
+    const res = await fetch("/api/playlists");
+    const { items } = await res.json();
+    setList(items);
+  };
   return (
     <div className="container">
       <Head>
@@ -22,6 +31,23 @@ export default function Home() {
             Signed in as {session.user?.name} <br />
             <button onClick={() => signOut()}>Sign out</button>
             <a href="/api/playlists">Playlists</a>
+            <button onClick={() => getMyPlaylists()}>
+              Get all my playlists
+            </button>
+            {list.map((item: PlaylistType) => (
+              <div key={item.id}>
+                <h1>{item.name}</h1>
+                <img
+                  src={
+                    (item.images &&
+                      item.images.length >= 0 &&
+                      item.images[0].url) ||
+                    ""
+                  }
+                  width="100"
+                />
+              </div>
+            ))}
           </>
         ) : (
           <>
