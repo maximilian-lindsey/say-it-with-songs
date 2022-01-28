91fab712c9a5d2450ee51e7b6a5eedf053ce4c00
Maximilian Lindsey <max@lindsey.digital>
2022-01-28 19:59:54 +0100

    Install packages and change homepage

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Say It With Songs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Say It With Songs" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Say it with songs by generating Spotify playlists
         </p>
       </main>
 
