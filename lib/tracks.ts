3560565fac7c674e0b92876c5292eec697cbb56f
Maximilian Lindsey <max@lindsey.digital>
2022-01-30 23:36:15 +0100

    Speed up search query

--- lib/tracks.ts
+++ lib/tracks.ts
@@ -1,7 +1,4 @@
-import { signIn } from "next-auth/react";
 import { Tracks, Track } from "../pages/api/search";
-
-type ResolvedTracks = Track[];
 
 type Words = string[];
 
@@ -19,32 +16,29 @@ export const getWordCombinations = (words: Words) => {
 export const getTracks = async (words: Words) => {
   const wordCombinations = getWordCombinations(words);
 
-  const resolvedTracks = [] as ResolvedTracks;
+  const tracks = await Promise.all(
+    wordCombinations.map((word) =>
+      fetch(`/api/search?q="${word}"`)
+        .then((res) => res.json())
+        .then((res: Tracks) => {
+          const filteredTracks = res.tracks.items.filter(
+            (track) => track.name.toLowerCase() === word.toLowerCase()
+          );
+          if (filteredTracks.length > 0) {
+            return filteredTracks.sort(
+              (a, b) => b.popularity - a.popularity
+            )[0];
+          }
+          return null;
+        })
+    )
+  );
 
-  for (const word of wordCombinations) {
-    const res = await fetch(`/api/search?q="${word}"`);
-    if (res.status === 200) {
-      const { tracks } = (await res.json()) as Tracks;
-      const filteredTracks = tracks.items.filter(
-        (track) => track.name.toLowerCase() === word.toLowerCase()
-      );
-      if (filteredTracks.length > 0) {
-        resolvedTracks.push(
-          filteredTracks.sort((a, b) => b.popularity - a.popularity)[0]
-        );
-      }
-    } else {
-      console.error(res);
-      if (res.status === 401) {
-        signIn();
-      }
-      break;
-    }
-  }
-  return resolvedTracks;
+  const filteredTracks = tracks.filter((track) => track) as Track[];
+  return filteredTracks;
 };
 
-export const buildInputFromTracks = (words: Words, tracks: ResolvedTracks) => {
+export const buildInputFromTracks = (words: Words, tracks: Track[]) => {
   const input = words.join(" ").toLowerCase();
   const selectedTracks = [];
   let phrase = "";
