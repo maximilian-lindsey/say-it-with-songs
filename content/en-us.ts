3742980da76914146a1d86cd8f0890e8d079bbd8
Maximilian Lindsey <max@lindsey.digital>
2022-01-31 23:02:28 +0100

    Fix typos

--- content/en-us.ts
+++ content/en-us.ts
@@ -1,12 +1,12 @@
 export const copy = {
   header: {
     title: "Say It With Songs",
-    description: "Create a Spotify playlists based on your words",
+    description: "Create a Spotify playlist based on your words",
   },
   login: {
     signin: {
       title: "Sign in with",
-      description: `You need to log in, so we can search Spotify for songs and save the playlist to your account.`,
+      description: `You need to sign in so we can search Spotify for songs and save the playlist to your account.`,
     },
     signout: {
       title: "Sign out",
