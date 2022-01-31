bbf312f63b5071cac91a07405f05d0f6af338f37
Maximilian Lindsey <max@lindsey.digital>
2022-01-31 20:01:26 +0100

    Add scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
-import '../styles/globals.css'
-import {SessionProvider} from 'next-auth/react';
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 
-function Application({Component, pageProps: {session, ...pageProps}}: AppProps) {
-  return <SessionProvider session={session}>
-    <Component {...pageProps} />
-  </SessionProvider>
+function Application({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
-export default Application
+export default Application;
